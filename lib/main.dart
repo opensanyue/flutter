f58efee4f3ecaa096d0da751743a2831538878c6
opensanyue <opensanyue@163.com>
2019-09-01 16:03:52 +0800

    一个flutter基本结构

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main()=>runApp(App());
+
+class App extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: '第一个例子',
+      home: Scaffold(
+        appBar: AppBar(title: Text('第一个flutter'),),
+        body: Center(child: Text('这是我们的第一个flutter例子'),),
+        ),
+    );
+  }
+}
