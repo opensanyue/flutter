eecdfe050c4575a014e70d2844ce7de367213f78
opensanyue <opensanyue@163.com>
2019-09-03 14:54:04 +0800

    image基本入门

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(App());
+void main()=>runApp(MyApp());
 
-class App extends StatelessWidget{
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title: '第一个例子',
-      home: Scaffold(
-        appBar: AppBar(title: Text('第一个flutter例子'),),
-        body: Center(child: Text('这是我们的第一个flutter例子'),),
-        ),
+          title:'image基本',
+          home:Scaffold(
+            appBar:AppBar(title: Text('image基本入门'),),
+            body:Center(
+              child: Container(
+                child:new Image.network(
+                      'http://hbimg.b0.upaiyun.com/2095ada981b2291ec8870e7a3f7459be99ca88e63848-efWN2F_fw658',
+                fit: BoxFit.none,
+                color: Colors.yellow,
+                colorBlendMode: BlendMode.darken,
+                repeat:ImageRepeat.repeatX,
+                ) ,
+                width: 300.0,
+                height: 400.0,
+                color: Colors.red,
+                ),
+            ),
+             ),
     );
   }
 }
