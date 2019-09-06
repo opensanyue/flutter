cdafccb9c09163ae8ae8be4d6f4b991a456ffdd6
opensanyue <opensanyue@163.com>
2019-09-06 11:17:37 +0800

    关于row的基本入门

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(App());
+void main()=>runApp(MyApp());
 
-class App extends StatelessWidget{
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title: '第一个例子',
-      home: Scaffold(
-        appBar: AppBar(title: Text('第一个flutter例子'),),
-        body: Center(child: Text('这是我们的第一个flutter例子'),),
-        ),
-    );
+      title: 'row基本入门',
+      home:Scaffold(
+        appBar:AppBar(title:Text('row基本入门')),
+        body:Row(
+          children: <Widget>[
+              RaisedButton(
+              onPressed: (){},
+              color: Colors.red,
+              child: Text('按钮1'),
+              ),
+            Expanded(child: 
+             RaisedButton(
+              onPressed: (){},
+              color: Colors.teal,
+              child: Text('按钮1'),
+              ),
+              ), 
+              RaisedButton(
+              onPressed: (){},
+              color: Colors.red,
+              child: Text('按钮1'),
+              ),
+          ],
+        )
+      ),
+
+
+      );
   }
 }
