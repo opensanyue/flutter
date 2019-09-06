f54f8b2f32957bc24d62154eac86755257bed1d6
opensanyue <opensanyue@163.com>
2019-09-06 11:46:27 +0800

    column基本入门

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(App());
+void main()=>runApp(MyApp());
 
-class App extends StatelessWidget{
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      title: '第一个例子',
+      title: 'column基本入门',
       home: Scaffold(
-        appBar: AppBar(title: Text('第一个flutter例子'),),
-        body: Center(child: Text('这是我们的第一个flutter例子'),),
-        ),
+        appBar: AppBar(title: Text('column基本入门'),),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+          Expanded(child: Text('这是一条数据1',style: TextStyle(backgroundColor: Colors.yellow,),),)  ,
+          Expanded(child: Text('这是一条数据1,这是一条数据1这是一条数据1,这是一条数据1这是一条数据1,这是一条数据1这是一条数据1,这是一条数据1',style: TextStyle(backgroundColor: Colors.indigo,),),
+          ),  
+            Text('这是一条数据1,这是一条数据1',style: TextStyle(backgroundColor: Colors.green,),),
+          ],
+          ),
+      ),
     );
   }
 }
